e05aa4bb7967c3f5e10e3df3f2bf873c41c88a78
Carlos Cardoso <cvcardos5@gmail.com>
2023-11-04 09:35:29 -0300

    Seller entity

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,18 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(2, "Carlos", "email@gmail.com", new Date(), 5020.99, obj);
+
+        System.out.println(seller);
     }
 }
